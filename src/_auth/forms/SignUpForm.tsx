205f2e45be96777e363a124b36847f5430b7f0fa
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-06 14:13:29 +0200

    style form and save changes

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -30,9 +30,11 @@ const SignUpForm = () => {
       <Form {...form}>
         <div className="sm:w-420 flex-center flex-col">
           <img src="/assets/images/logo.svg" alt="logo"/>
-        </div>
-        
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12"> Create a new account</h2>
+          <p className="text-light-3 small-medium md:base-regular">To use TravelMeet enter your credentials</p>
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
         <FormField
           control={form.control}
           name="username"
@@ -51,6 +53,7 @@ const SignUpForm = () => {
         />
         <Button type="submit">Submit</Button>
       </form>
+      </div>
       </Form>
   )
 }
