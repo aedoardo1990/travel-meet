271f339515d3d787e8fd1f13d6b4718e5a314370
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-06 14:00:08 +0200

    add logo to signup form and style form

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -5,31 +5,34 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form"
+import { SignUpValidation } from "@/lib/validation"
  
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
-
 const SignUpForm = () => {
   
    // 1. Define your form.
-   const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+   const form = useForm<z.infer<typeof SignUpValidation>>({
+    resolver: zodResolver(SignUpValidation),
     defaultValues: {
-      username: "",
+      name: '',
+      username: '',
+      email: '',
+      password: '',
     },
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof SignUpValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
   return (
-    <div>
       <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="sm:w-420 flex-center flex-col">
+          <img src="/assets/images/logo.svg" alt="logo"/>
+        </div>
+        
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
           name="username"
@@ -48,8 +51,7 @@ const SignUpForm = () => {
         />
         <Button type="submit">Submit</Button>
       </form>
-    </Form>
-    </div>
+      </Form>
   )
 }
 
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,9 @@
+import * as z  from "zod"
+
+export const SignUpValidation = z.object({
+    name: z.string().min(2, { message: 'Too short. Type at least 2 chars'}),
+    username: z.string().min(2, { message: 'Too short. Type at at least 2 chars'}),
+    email: z.string().email(),
+    password: z.string().min(8, { message: 'Too short. Type at at least 8 chars'}),
+  })
+  
