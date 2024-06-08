2ecde75ee8443bfe7a5e82f02af5670d066f9eb7
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-08 18:50:57 +0200

    save changes

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -131,7 +131,7 @@ const SignUpForm = () => {
 
           <p className="text-small-regular text-light-2 text-center mt-2">
             Already have an account?
-            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log In</Link>
+            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
           </p>
         </form>
       </div>
