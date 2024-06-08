fb183478f5f2133a451850a3d2d5e25599709146
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-08 18:07:29 +0200

    create SignInForm

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,9 +1,13 @@
-import * as z  from "zod"
+import * as z from "zod"
 
 export const SignUpValidation = z.object({
-    name: z.string().min(2, { message: 'Too short. Type at least 2 chars'}),
-    username: z.string().min(2, { message: 'Too short. Type at at least 2 chars'}),
-    email: z.string().email(),
-    password: z.string().min(8, { message: 'Too short. Type at at least 8 chars'}),
-  })
-  
+  name: z.string().min(2, { message: 'Too short. Type at least 2 chars' }),
+  username: z.string().min(2, { message: 'Too short. Type at at least 2 chars' }),
+  email: z.string().email(),
+  password: z.string().min(8, { message: 'Too short. Type at at least 8 chars' }),
+})
+
+export const SignInValidation = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, { message: 'Too short. Type at at least 8 chars' }),
+})
