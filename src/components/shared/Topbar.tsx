f677ea015778f5a79e99b37d68891c525b9eb06f
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-11 15:33:13 +0200

    create leftsidebar

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'; 
+import { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../ui/button';
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations';
@@ -10,39 +10,40 @@ const Topbar = () => {
     const navigate = useNavigate();
     const { user } = useUserContext();
 
-    useEffect (() => {
-        if(isSuccess) navigate(0);
+    useEffect(() => {
+        if (isSuccess) navigate(0);
     }, [isSuccess])
-  return (
-    <section className='topbar'>
-        <div className='flex-between py-4 px-5'>
-            <Link to="/" className='flex gap-3 items-center'>
-            <img 
-            src='/assets/images/logo.svg'
-            alt='logo'
-            width={130}
-            height={325}
-            />
-            </Link>
 
-            <div className='flex gap-4'>
-                <Button variant='ghost' className='shad-button_ghost'
-                onClick={() => signOut()}>
-                    <img src='/assets/icons/logout.svg' alt='logout' />
-                </Button>
-                {/*code for rounded profile icon view*/}
-                <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
-                <img 
-                src={user.imageUrl || '/assets/images/profile-placeholder.svg'}
-                alt='profile'
-                className='h-8 w-8 rounded-full'
-                />
+    return (
+        <section className='topbar'>
+            <div className='flex-between py-4 px-5'>
+                <Link to="/" className='flex gap-3 items-center'>
+                    <img
+                        src='/assets/images/logo.svg'
+                        alt='logo'
+                        width={130}
+                        height={325}
+                    />
                 </Link>
-            </div>
-        </div>
 
-    </section>
-  )
+                <div className='flex gap-4'>
+                    <Button variant='ghost' className='shad-button_ghost'
+                        onClick={() => signOut()}>
+                        <img src='/assets/icons/logout.svg' alt='logout' />
+                    </Button>
+                    {/*code for rounded profile icon view*/}
+                    <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
+                        <img
+                            src={user.imageUrl || '/assets/images/profile-placeholder.svg'}
+                            alt='profile'
+                            className='h-8 w-8 rounded-full'
+                        />
+                    </Link>
+                </div>
+            </div>
+
+        </section>
+    )
 }
 
 export default Topbar
