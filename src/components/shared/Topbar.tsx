3454e0d4e6e7f21197826f0a71cfa600513dd356
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-11 12:24:01 +0200

    create rounded profile icon in topbar

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,8 +1,18 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react'; 
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../ui/button';
+import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations';
+import { useUserContext } from '@/context/AuthContext';
 
 
 const Topbar = () => {
+    const { mutate: signOut, isSuccess } = useSignOutAccount();
+    const navigate = useNavigate();
+    const { user } = useUserContext();
+
+    useEffect (() => {
+        if(isSuccess) navigate(0);
+    }, [isSuccess])
   return (
     <section className='topbar'>
         <div className='flex-between py-4 px-5'>
@@ -16,9 +26,18 @@ const Topbar = () => {
             </Link>
 
             <div className='flex gap-4'>
-                <Button variant='ghost' className='shad-button_ghost'>
+                <Button variant='ghost' className='shad-button_ghost'
+                onClick={() => signOut()}>
                     <img src='/assets/icons/logout.svg' alt='logout' />
                 </Button>
+                {/*code for rounded profile icon view*/}
+                <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
+                <img 
+                src={user.imageUrl || '/assets/images/profile-placeholder.svg'}
+                alt='profile'
+                className='h-8 w-8 rounded-full'
+                />
+                </Link>
             </div>
         </div>
 
