fcdbb81ee50bb84816e2faa8b8fe7ef2a33222d1
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-18 18:32:52 +0200

    create additional private routes under app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './globals.css'
 import SignInForm from './_auth/forms/SignInForm'
 import SignUpForm from './_auth/forms/SignUpForm'
@@ -11,15 +11,23 @@ const App = () => {
   return (
     <main className='flex h-screen'>
       <Routes>
-      {/*Public routes */}
-      <Route element={<AuthLayout/>}>
-      <Route path='/sign-in' element={<SignInForm/>}/>
-      <Route path='/sign-up' element={<SignUpForm/>}/>
-      </Route>
-      {/*Private routes */}
-      <Route element={<RootLayout/>}>
-      <Route index element={<Home/>}/>
-      </Route>
+        {/*Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SignInForm />} />
+          <Route path='/sign-up' element={<SignUpForm />} />
+        </Route>
+        {/*Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path='/explore' element={<Explore />}/>
+          <Route path='/saved' element={<Saved/>}/>
+          <Route path='/all-users' element={<AllUsers />}/>
+          <Route path='/create-post' element={<CreatePost />}/>
+          <Route path='/update-post/:id' element={<EditPost />}/>
+          <Route path='/posts/:id' element={<PostDetails />}/>
+          <Route path='/profile/:id' element={<Profile />}/>
+          <Route path='/update-profile/:id' element={<UpdateProfile />}/>
+        </Route>
       </Routes>
       <Toaster />
     </main>
