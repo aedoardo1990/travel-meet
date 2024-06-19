56f166af63bd12cae633e101e709aa12acc0dba5
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-19 14:09:27 +0200

    connect FileUploader to CreatePost form

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -20,7 +20,7 @@ const formSchema = z.object({
     }),
 })
 
-const PostForm = () => {
+const PostForm = ({ post }) => {
     // 1. Define your form.
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -60,7 +60,10 @@ const PostForm = () => {
                         <FormItem>
                             <FormLabel className="shad-form_label">Add Photos</FormLabel>
                             <FormControl>
-                                <FileUploader />
+                                <FileUploader 
+                                fieldChange={field.onChange}
+                                mediaUrl={post?.imageUrl}
+                                />
                             </FormControl>
                             <FormMessage className="shad-form_message" />
                         </FormItem>
