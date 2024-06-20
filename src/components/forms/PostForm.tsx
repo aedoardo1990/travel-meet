da9c424a07620c1b16acc0d388e2544bbf955ba3
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-20 15:40:53 +0200

    solve bug of textarea component in PostForm.tsx

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -2,35 +2,31 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 import { Button } from "@/components/ui/button"
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { FileUploader } from "../shared/FileUploader"
+import { PostValidation } from "@/lib/validation"
+import { Models } from 'appwrite'
+import { Textarea } from "@/components/ui/textarea"
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-})
+type PostFormProps = {
+    post?: Models.Document;
+}
 
-const PostForm = ({ post }) => {
+const PostForm = ({ post }: PostFormProps) => {
     // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const form = useForm<z.infer<typeof PostValidation>>({
+        resolver: zodResolver(PostValidation),
         defaultValues: {
-            username: "",
+            caption: post ? post?.caption: "",
+            file: [],
+            location: post ? post?.location : "",
+            tags: post ? post.tags.join(',') : ''
         },
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    function onSubmit(values: z.infer<typeof PostValidation>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         console.log(values)
@@ -38,7 +34,9 @@ const PostForm = ({ post }) => {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
+            <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-9 w-full max-w-5xl">
                 <FormField
                     control={form.control}
                     name="caption"
@@ -46,7 +44,10 @@ const PostForm = ({ post }) => {
                         <FormItem>
                             <FormLabel className="shad-form_label">Caption</FormLabel>
                             <FormControl>
-                                <textarea className="shad-textarea custom-scrollbar" {...field} />
+                                <Textarea 
+                                className="shad-textarea custom-scrollbar" 
+                                {...field} 
+                                />
                             </FormControl>
 
                             <FormMessage className="shad-form_message" />
@@ -76,7 +77,7 @@ const PostForm = ({ post }) => {
                         <FormItem>
                             <FormLabel className="shad-form_label">Add Location</FormLabel>
                             <FormControl>
-                                <Input type="text" className="shad-input" />
+                                <Input type="text" className="shad-input" {...field}/>
                             </FormControl>
                             <FormMessage className="shad-form_message" />
                         </FormItem>
@@ -93,6 +94,7 @@ const PostForm = ({ post }) => {
                                     type="text"
                                     className="shad-input"
                                     placeholder="JS, React, NextJS"
+                                    {...field}
                                 />
                             </FormControl>
                             <FormMessage className="shad-form_message" />
