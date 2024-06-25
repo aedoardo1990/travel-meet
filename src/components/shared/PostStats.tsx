6233ee86be75a7f7bd8c9528b25a1736c50708c4
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-25 16:10:04 +0200

    add conditional rendering in PostStats to handle likes

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,5 +1,6 @@
 import { useUserContext } from '@/context/AuthContext';
 import { useDeleteSavedPost, useLikePost, useSavePost } from '@/lib/react-query/queriesAndMutations';
+import { checkIsLiked } from '@/lib/utils';
 import { Models } from 'appwrite'
 import { useState } from 'react';
 
@@ -12,6 +13,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   const likesList = post.likes.map((user: Models.Document) => user.$id)
 
   const [likes, setLikes] = useState(likesList);
+  const [ isSaved, setIsSaved ] = useState(false); //to know if post was saved
 
   const { mutate: likePost } = useLikePost();
   const { mutate: savePost } = useSavePost();
@@ -19,11 +21,18 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   // to retrieve current user
   const { data: currentUser } = useUserContext();
 
+  const handleLikePost = () => {}
+
+  const handleSavePost = () => {}
+
   return (
     <div className='flex justify-between items-center z-20'>
       <div className='flex gap-2 mr-5'>
         <img
-          src='/assets/icons/like.svg'
+          src={`${checkIsLiked(likes, userId) 
+            ? '/assets/icons/liked.svg'
+            : 'assets/icons/like.svg'}
+            `}
           alt='like'
           width={20}
           height={20}
