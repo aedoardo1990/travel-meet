68261a960c244655693f18bc9914b669e2cf9225
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-25 10:29:30 +0200

    create savePost function to save newly created posts

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -212,3 +212,23 @@ export async function likePost(postId: string, likesArray: string[]){
     }
 }
 
+export async function savePost(postId: string, userId: string){
+    try {
+        const updatedPost = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.savesCollectionId,
+            ID.unique(), // instead of PostId cause it is new post
+            {
+                user: userId,
+                post: postId,
+            }
+        )
+
+        if (!updatedPost) throw Error;
+
+        return updatedPost;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
