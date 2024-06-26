79d6f1e27e35404d7b608f6a4caea64cf4b883db
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-26 18:23:41 +0200

    create Profile.tsx page

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from '@tanstack/react-query'
-import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount, likePost, savePost, deleteSavedPost, getCurrentUser, getPostById, updatePost, deletePost, getInfinitePosts, searchPosts,  getUsers } from '../appwrite/api'
+import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount, likePost, savePost, deleteSavedPost, getCurrentUser, getPostById, updatePost, deletePost, getInfinitePosts, searchPosts,  getUsers, getUserById } from '../appwrite/api'
 import { INewPost, INewUser, IUpdatePost } from '@/types'
 import { QUERY_KEYS } from './queryKeys';
 
@@ -120,6 +120,14 @@ export const useGetUsers = (limit?: number) => {
     return useQuery({
       queryKey: [QUERY_KEYS.GET_USERS],
       queryFn: () => getUsers(limit),
+    });
+  };
+
+  export const useGetUserById = (userId: string) => {
+    return useQuery({
+      queryKey: [QUERY_KEYS.GET_USER_BY_ID, userId],
+      queryFn: () => getUserById(userId),
+      enabled: !!userId,
     });
   };
 
