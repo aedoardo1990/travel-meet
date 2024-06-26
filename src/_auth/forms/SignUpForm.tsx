b2391c86ee7cfe2ee66eaef63e182316e456c915
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-26 17:59:57 +0200

    fix bug in SignUpForm

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -122,7 +122,7 @@ const SignUpForm = () => {
             )}
           />
           <Button type="submit" className="shad-button_primary">
-            {isCreatingAccount ? (
+            {isCreatingAccount || isSigningIn || isUserLoading? (
               <div className="flex-center gap-2">
                 <Loader /> Loading...
               </div>
