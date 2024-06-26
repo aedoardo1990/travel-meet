27a24a2ace4dfcb446ebc59e7a7498b42d838df1
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-26 13:51:12 +0200

    create SearchResults component

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import Loader from './Loader'
+import { Models } from 'appwrite';
+import GridPostList from './GridPostList';
 
-const SearchResults = () => {
+type SearchResultsProps = {
+  isSearchFetching: boolean;
+  searchedPosts: Models.Document[];
+}
+
+const SearchResults = ({ isSearchFetching, searchedPosts}: SearchResultsProps) => {
+  if(isSearchFetching) return <Loader />
+
+  if(searchedPosts && searchedPosts.documents.length > 0) {
+    return (
+      <GridPostList posts={searchedPosts.documents}/>
+    )
+  }
   return (
-    <div>SearchResults</div>
+
+    <p className='text-ligth-4 mt-10 text-center w-full'>
+      No results found
+    </p>
   )
 }
 
