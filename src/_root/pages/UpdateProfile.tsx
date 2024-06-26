609863f48607091f80a9a312e31f30d66ef740ee
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-06-26 18:54:37 +0200

    try to fix bug at useUpdateUser.tsx

--- src/_root/pages/UpdateProfile.tsx
+++ src/_root/pages/UpdateProfile.tsx
@@ -39,8 +39,7 @@ const UpdateProfile = () => {
 
   // Queries
   const { data: currentUser } = useGetUserById(id || "");
-  const { mutateAsync: updateUser, isLoading: isLoadingUpdate } =
-    useUpdateUser();
+  const { mutateAsync: updateUser, isLoading: isLoadingUpdate } = useUpdateUser();
 
   if (!currentUser)
     return (
