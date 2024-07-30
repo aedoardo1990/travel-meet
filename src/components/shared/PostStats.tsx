fd656e8fa858131136421f766e969e9f7500113b
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-07-30 17:54:59 +0200

    add comment icon to poststats.tsx

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -56,36 +56,47 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   }
 
   return (
-    <div className='flex justify-between items-center z-20'>
-      <div className='flex gap-2 mr-5'>
-        <img
-          src={`${checkIsLiked(likes, userId)
-            ? '/assets/icons/liked.svg'
-            : '/assets/icons/like.svg'
-          }`}
-          alt='like'
-          width={20}
-          height={20}
-          onClick={handleLikePost}
-          className='cursor-pointer'
-        />
-        <p className='small-medium lg:base-medium'>{likes.length}</p>
-      </div>
+    <div>
+      <div className='flex justify-between items-center z-20'>
+        {/*Like Post*/}
+        <div className='flex gap-2 mr-5'>
+          <img
+            src={`${checkIsLiked(likes, userId)
+              ? '/assets/icons/liked.svg'
+              : '/assets/icons/like.svg'
+              }`}
+            alt='like'
+            width={20}
+            height={20}
+            onClick={handleLikePost}
+            className='cursor-pointer'
+          />
+          <img
+            src={'/assets/icons/comment.svg'}
+          />
+        </div>
+        {/*Comment Post*/}
 
-      <div className='flex gap-2'>
-      {isSavingPost || isDeletingSaved ? <Loader /> : <img
-          src={isSaved
-            ? '/assets/icons/saved.svg'
-            : '/assets/icons/save.svg'
-          }
-          alt='like'
-          width={20}
-          height={20}
-          onClick={handleSavePost}
-          className='cursor-pointer'
-        />}
+        {/*Save Post*/}
+        <div className='flex gap-2'>
+          {isSavingPost || isDeletingSaved ? <Loader /> : <img
+            src={isSaved
+              ? '/assets/icons/saved.svg'
+              : '/assets/icons/save.svg'
+            }
+            alt='like'
+            width={20}
+            height={20}
+            onClick={handleSavePost}
+            className='cursor-pointer'
+          />}
+        </div>
+      </div>
+      <div className='ml-1'>
+        <p className='small-medium lg:base-medium'>{likes.length} likes</p>
       </div>
     </div>
+
   )
 }
 
